e80258e6c631bb5c464cfbd14f921c4f05d6ebf2
kionae <kionae@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 21:16:24 +0000

    * Security vulnerability fixes
    git-svn-id: https://plugins.svn.wordpress.org/my-wish-list/tags/1.4.2@1133970 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,121 @@
+=== My Wish List ===
+Contributors: kionae
+Donate link: http://www.nlb-creations.com/donate
+Tags: item lists, wish list, wishlist
+Requires at least: 3.2.0
+Tested up to: 4.1
+Stable tag: trunk
+
+This plugin allows you to create wish lists for your website, and display them on any post or page with simple shortcode.
+
+== Description ==
+
+This plugin allows you to create wish lists for your website, and display them on any post or page with simple shortcode.  Include Item names, prices, sizes, 
+colors, links, and photos so everyone knows what you want/need.
+
+== Installation ==
+
+1. Upload plugin .zip file to the `/wp-content/plugins/` directory and unzip.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. A new post type called "Wish Lists" will appear in the admin menu.  Use this to create your wish lists.
+4. Use the shortcode [wishlist id=xx] in your posts and pages to display your wishlists.  
+
+== Frequently Asked Questions ==
+
+= How do I display my wishlist? =
+
+There are two ways.
+
+1) Simply link to it.  A new page exists on your site for each list you create.  If you want to customize how your list is laid out, this is the option for you.  
+Copy the template file /my-wish-list/templates/single-wishlist.php into your active theme's directory and edit away.
+
+2) Use the shortcode [wishlist id=xx] in any page or post, where id is the post id of the wishlist you created.  This shortcode appears in the upper-right
+box on the edit form.  Just copy/paste it into your post.
+
+= Can visitors reserve items to purchase from my wishlist? =
+
+Yes.  This can enabled in the Wishlist settings box in any wishlist you create.  Information about promised items will appear in the edit page for the wishlist.
+
+= Can visitors purchase items through this plugin? =
+
+No.  This is not an e-commerce plugin.  It only allows you to list items you need/want, and tell people where they can buy them for you. 
+
+= Can I change the CSS styles for the wishlist? =
+
+Yes.  Just copy the my-wish-list/styles/wishlist.css file into your active theme's directory and make all the changes you like.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial release
+
+= 0.0.2 =
+* Bug fix for wishlists save with no items added
+
+= 1.0 =
+* Moved all CSS to external file
+* Added code to allow override of default CSS
+* Added ability for donors to reserve items
+* Added additional settings to each wishlist created to make them more customizable
+
+= 1.1 =
+* Fixed some compatibility issues with WordPress 3.6
+* Added a template file for Wishlists that can be customized 
+* Plugin now sends an email to the site admin when an item is promised
+
+= 1.2 =
+* Minor fix in the single-wishlist.php template file
+
+= 1.3 =
+* Added item name to emails
+
+= 1.3.1 =
+* Fix for admin emails
+
+= 1.4 =
+* Fixed a bug that would not allow users to display links that start with https://
+* Verified compatibility with WordPress 4.0
+
+= 1.4.1 =
+* Fixed a minor PHP error that occurred with no list items were present when a list is saved 
+
+= 1.4.2 =
+* Security vulnerability fixes 
+
+== Upgrade Notice ==
+
+= 0.1 =
+* Initial release
+
+= 0.0.2 =
+* Bug fix for wishlists save with no items added
+
+= 1.0 =
+* Moved all CSS to external file
+* Added code to allow override of default CSS
+* Added ability for donors to reserve items
+* Added additional settings to each wishlist created to make them more customizable
+
+= 1.1 =
+* Fixed some compatibility issues with WordPress 3.6
+* Added a template file for Wishlists that can be customized 
+* Plugin now sends an email to the site admin when an item is promised
+
+= 1.2 =
+* Minor fix in the single-wishlist.php template file
+
+= 1.3 =
+* Added item name to emails
+
+= 1.3.1 =
+* Fix for admin emails
+
+= 1.4 =
+* Fixed a bug that would not allow users to display links that start with https://
+* Verified compatibility with WordPress 4.0
+
+= 1.4.1 =
+* Fixed a minor PHP error that occurred with no list items were present when a list is saved
+
+= 1.4.2 =
+* Security vulnerability fixes
---
+++ templates/single-wishlist.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * The Template for displaying all single playlist posts.  Based on TwentyEleven.
+ */
+
+get_header(); ?>
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<nav id="nav-single">
+						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
+						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
+						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
+					</nav><!-- #nav-single -->
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php if ( 'post' == get_post_type() ) : ?>
+						<div class="entry-meta">
+							<?php twentyeleven_posted_on(); ?>
+						</div><!-- .entry-meta -->
+						<?php endif; ?>
+					</header><!-- .entry-header -->
+					
+					<div class="entry-content">
+						<!-- custom wishlist output -->
+						
+						<?php 
+						//get the metadata
+						$wishlist_instructions_link = get_post_meta($post->ID,'wishlist_instructions_link',true);
+						$wishlist_show_form = get_post_meta($post->ID,'wishlist_show_form',true);
+						$wishlist_show_donor = get_post_meta($post->ID,'wishlist_show_donor',true);
+
+						//thank you message for donors
+						if(isset($_POST['wish_donor_thank_you'])) {
+							echo '<div class="wishlist-thanks"> Thank you for pledging to get '.sanitize_text_field($_POST['wish_donor_donation']).' for us!  ';
+							if($wishlist_instructions_link != '') {
+								echo '<a href="'.$wishlist_instructions_link.'">Click here</a> for instructions on purchasing and sending the item.';
+							}
+							echo '</div>';
+							unset($_POST['wish_donor_thank_you']);
+						}
+						
+						?>
+						
+						<div class="wishlist-desc"><?php the_content();?></div>
+					
+						<?php 
+						//output the list
+						$wishmeta = get_post_meta(get_the_ID(), 'my_wishes', true);
+						foreach($wishmeta as $i => $meta):
+						?>
+						<div class="wishlist-item">
+							<h3><?php echo $meta['wishitem']; ?></h3>
+									
+							<div class="wishlist-image-form">
+								<?php if($meta['wishdonorname'] != '' || $meta['wishdonoremail'] != ''): ?>
+									<?php if($wishlist_show_donor == "show"): ?>
+											<span class="wishlist-promised">Item Promised by <?php echo $meta['wishdonorname']; ?>!</span><br />
+									<?php else: ?>
+											<span class="wishlist-promised">Item Promised!</span><br />
+									<?php endif; ?>
+								<?php endif; ?>
+									
+								<?php if($meta['wishfilename'] != ''): ?>
+									<?php $thumb = wp_get_attachment_image_src( $meta['wishfilename'], 'medium'); ?>
+									<img src="<?php echo $thumb[0]; ?>" />
+									<br />
+								<?php endif; ?>
+								
+								<?php if($meta['wishdonorname'] == '' && $meta['wishdonoremail'] == '' && $wishlist_show_form == "show"): ?>
+								I would like to get this item for you!  Tell me how!
+								<form method="post" action="" class="wishlist-form">
+									<table>
+										<tr><td><span class="wishlist-form-label">Name:</span></td><td><input name="wishdonorname" type="text" class="wishlist-form-input" /></td></tr>
+										<tr><td><span class="wishlist-form-label">Email:</span></td><td><input name="wishdonoremail" type="text" class="wishlist-form-input" /></td></tr>
+										
+										<tr><td colspan="2" class="wishlist-double-span">
+										
+										<input type="hidden" name="wishitem" value="<?php echo $meta['wishitem']; ?>" />
+										<input type="hidden" name="post_id" value="<?php the_ID(); ?>" />
+										<input type="hidden" name="wishindex" value="<?php echo $i; ?>" />
+										<input type="hidden" name="wish_donor_add_update" value="1" />
+										<input type="submit" value="Submit" class="wishlist-form-submit-button" />
+										</td></tr>
+									</table>
+								</form>
+								<?php endif; ?>
+							</div>
+									
+							<?php if($meta['wishprice'] != ''): ?>
+								<span class="wishlist-label">Price:</span> <?php echo $meta['wishprice']; ?><br />
+							<?php endif; ?>
+							
+							<?php if($meta['wishsize'] != ''): ?>
+								<span class="wishlist-label">Size:</span> <?php echo $meta['wishsize']; ?><br />
+							<?php endif; ?>
+							
+							<?php if($meta['wishcolor'] != ''): ?>
+								<span class="wishlist-label">Color:</span> <?php echo $meta['wishcolor']; ?><br />
+							<?php endif; ?>
+							
+							<?php if($meta['wishstore'] != ''): ?>
+								<span class="wishlist-label">Available at:</span> <?php echo $meta['wishstore']; ?><br />
+							<?php endif; ?>
+									
+							<?php if($meta['wishlink'] != ''): ?>
+								<?php 
+									if(!stristr($meta['wishlink'], 'http://') && !stristr($meta['wishlink'], 'https://')) {
+										$meta['wishlink'] = 'http://'.$meta['wishlink'];
+									}
+								?>
+									<span class="wishlist-label">Link:</span> <a href="<?php echo $meta['wishlink']; ?>"><?php echo $meta['wishlink']; ?></a><br />
+							<?php endif; ?>
+								
+								
+						</div>
+									
+						<hr class="wishlist-hr" />
+						<?php endforeach; ?>
+						 
+						<!-- /custom wishlist output -->
+						
+						<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+					</div><!-- .entry-content -->
+					</article>
+
+					<?php //comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
